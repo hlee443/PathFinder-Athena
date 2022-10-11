c8c065f980c785af00f69a26152bc615aa2f588d
Ivy Lee <hlee443@my.bcit.ca>
2022-10-10 18:00:45 -0700

    highlighted word will automatically pull up existing definition

--- dictionary-app/pages/index.jsx
+++ dictionary-app/pages/index.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+
 export default function Home() {
   const [word, setWord] = useState("");
   const [wordInfo, setWordInfo] = useState(null);
@@ -11,6 +12,7 @@ export default function Home() {
   const fetchInfo = async (e) => {
     e.preventDefault();
     try {
+      const word = window.getSelection().toString();
       const res = await axios.get(`/api/info`, {
         params: { word },
       });
@@ -19,10 +21,23 @@ export default function Home() {
       // split the response string into an array using regex
       const newDefinition = definition.split(/1. |2. | 3. /);
       setWordInfo(newDefinition);
+      // console.log(window.getSelection().toString());
     } catch (err) {
       console.log(err);
     }
   };
+
+  useEffect(() => {
+    // add event listener to the document
+    document.addEventListener("mouseup", fetchInfo);
+
+    // remove event listener when component unmounts
+    return () => {
+      document.removeEventListener("mouseup", fetchInfo);
+    };
+  }, []);
+
+
   return (
     <div className="bg-bc flex flex-col items-center relative min-h-screen ">
       <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
@@ -31,8 +46,24 @@ export default function Home() {
       <h3 className="text-lightOrange text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
         Check Meaning of any word
       </h3>
+      <p className="flex flex-col text-primary text-raleway mt-12 w-3/6 h-4/5  md:flex-col md:w-4/6 md:h-full md:mb-12">
+        The term “full stack developer” originated during the early days of the
+        web, when websites were small and uncomplicated enough to allow a single
+        person to tackle every aspect of site-building. But in the decades since
+        those initial days, the web has grown ever more complex. The rise of
+        machine learning, predictive computing, and responsive design has made
+        it challenging — but not impossible! — for a single developer to handle
+        every aspect of building and designing a site or application. Today,
+        modern businesses often rely on entire teams of developers to operate
+        network equipment, work with virtual machines, and manage enormous
+        databases. It takes time to develop a comprehensive, nuts-and-bolts
+        understanding of all these emerging technologies. The developers who do
+        so are, for that reason, versatile enough to shift fluidly between front
+        and back end development and take on any task that their team might need
+        them to tackle.{" "}
+      </p>
       <div className="flex flex-col justify-between items-center w-full md:items-center">
-        <form
+        {/* <form
           onSubmit={(e) => fetchInfo(e)}
           className="flex w-full justify-center md:flex-col md:w-5/6 "
         >
@@ -49,14 +80,14 @@ export default function Home() {
           >
             Search
           </button>
-        </form>
+        </form> */}
         {wordInfo && (
           <div className="flex flex-col text-primary text-raleway mt-12 w-3/6 h-4/5  md:flex-col md:w-4/6 md:h-full md:mb-12">
             <table className="bg-white w-full text-primary mb-8 md:text-sm md:mx-2">
               <thead className="font-raleway uppercase tracking-wide">
                 <tr>
                   <th className="border text-left px-4 py-4">
-                    <span className="text-secondary">{word}</span>
+                    <span className="text-secondary">{window.getSelection().toString()}</span>
                   </th>
                   <th className="border text-left px-4 py-4">
                     <span className="text-secondary">Definition</span>
