95d0636c9c06e6027c574ad6f52016c4ef3ba160
Ivy Lee <hlee443@my.bcit.ca>
2022-10-16 14:11:26 -0700

    create highlighter handler

---
+++ pages/handlers/features/highlighter.js
@@ -0,0 +1,7 @@
+// Highlighter handlers
+// - user action (mouseover)
+// - expected behaviour (highlight selected text in a different colour)
+
+export default function handleHighlighter(){
+    
+}
