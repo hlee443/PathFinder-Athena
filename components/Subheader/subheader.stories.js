585b127a786cc411db2c7c2c25d95b41e5fbac2d
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-17 12:05:42 -0700

    add text setting

--- components/Subheader/subheader.stories.js
+++ components/Subheader/subheader.stories.js
@@ -1,4 +1,5 @@
-import SubHeader from "./subheader";
+import SubHeader from "./SubHeader";
+import { text_data } from "../../styles/globals";
 
 export default {
     title: "SubHeader",
@@ -9,6 +10,8 @@ const Template = args => <SubHeader {...args} />
 
 export const Subheader = Template.bind({})
 Subheader.args = {
-    text: 'example',
-    size: 'big',
+    text: 'Subheader text',
+    size: text_data.h2.size,
+    line_height: text_data.line_height,
+    letter_spacing: text_data.letter_spacing
 }
