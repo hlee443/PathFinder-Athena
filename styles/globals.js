96153fb13fd0d0e3bc10ebf741f7921fa732b8cc
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-16 20:47:45 -0700

    initial commit

---
+++ styles/globals.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const text_data = {
+    line_height: "150%",
+    letter_spacing: "0.35em",
+    h1: {
+        size: "2.375rem"
+    },
+    h2: {
+        size: "1.5rem"
+    }
+}
+
+export const colors = {
+    
+}
+
+
