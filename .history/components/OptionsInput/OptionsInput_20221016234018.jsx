b001415c144890e32559deaf66b80144c9470b9d
cho196 <91933101+cho196@users.noreply.github.com>
2022-10-16 23:42:21 -0700

    move styling outside

--- .history/components/OptionsInput/OptionsInput_20221016234018.jsx
+++ .history/components/OptionsInput/OptionsInput_20221016234018.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const OptionsCont = styled.select`
+const OptionsInput = styled.select`
   border-radius: 2rem;
   border: 0.05rem solid #3e3e3e;
   padding: 1rem;
@@ -8,12 +8,12 @@ const OptionsCont = styled.select`
 
 export default function OptionsInput() {
   return (
-    <OptionsCont>
+    <OptionsInput>
       <option value="" disabled selected>
         Placeholder
       </option>
       <option></option>
       <option></option>
-    </OptionsCont>
+    </OptionsInput>
   );
 }
