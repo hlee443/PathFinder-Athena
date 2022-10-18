8139720ad8021d34e3034f306e894ab157d863c4
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-17 20:12:38 -0700

    test page

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -1,5 +1,18 @@
-import Header from "../components/Header"
+import styled from "styled-components";
+import NavBar from "../components/NavBar/NavBar";
+import Header from "../components/Header/Header";
+import SubHeader from "../components/SubHeader/SubHeader";
+import { Flexbox, Wrapper } from "../styles/globals";
 
-export default function Sandbox() { 
-return <Header text="header works"></Header>
+
+export default function Sandbox() {
+    return (
+        <Flexbox>
+            <NavBar></NavBar>
+            <Wrapper>
+                <Header text="Upload your study materials!"></Header>
+                <SubHeader text="Enter URL or upload files, we will make it easier to understand for you."></SubHeader>
+            </Wrapper>
+        </Flexbox>
+    )
 }
