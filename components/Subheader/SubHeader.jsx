c83aeee3184d5db024646fbeabba8ba207e46613
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-17 17:28:20 -0700

    change font-size

--- components/Subheader/SubHeader.jsx
+++ components/Subheader/SubHeader.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { text_data } from '../../styles/globals';
 
 const SubHeaderCont = styled.h2`
-    font-size: ${text_data.h1.size};
+    font-size: ${text_data.h2.size};
     color: ${props => props.color};
     line-height: ${text_data.line_height};
     letter-spacing: ${text_data.letter_spacing};
