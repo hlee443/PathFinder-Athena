3447b3caa774875753f34dfd94fbcb3ceca23292
cho196 <91933101+cho196@users.noreply.github.com>
2022-10-17 22:47:31 -0700

    basic bubble layout no styling

---
+++ components/Bubble/Bubble.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import Icon from "../Icon/Icon";
+import Header from "../Header/Header";
+import Input from "../Input/Input";
+import Subheader from "../Subheader/SubHeader";
+import Button from "../Button/Button";
+
+export default function Bubble({
+  width = "60rem",
+  type = "login",
+  height = "42rem",
+}) {
+  const Bubble = styled.div`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    border-radius: 2rem;
+    background-color: #fffef6;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 4rem;
+  `;
+  const ButtonCont = styled.div`
+    display: flex;
+    justify-content: space-between;
+  `;
+  const InputDiv = styled.div``;
+
+  return (
+    <Bubble width={width} height={height}>
+      <Icon></Icon>
+      <Header></Header>
+      {type === "login" && (
+        <InputDiv>
+          <Input type="email"></Input>
+          <Input type="password"></Input>
+        </InputDiv>
+      )}
+      <Subheader></Subheader>
+      <ButtonCont>
+        <Button></Button>
+        <Button></Button>
+      </ButtonCont>
+    </Bubble>
+  );
+}
