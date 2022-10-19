a0fa9562eb0cf204ef8669272e10338cf54f0a37
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-19 07:12:09 -0700

    layout comps

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -2,30 +2,34 @@ import styled from "styled-components";
 import NavBar from "../components/NavBar/NavBar";
 import Header from "../components/Header/Header";
 import SubHeader from "../components/SubHeader/SubHeader";
-import { Flexbox, Wrapper } from "../styles/globals";
+import { colors, Flexbox, Wrapper } from "../styles/globals";
 import TabBar from "../components/TabBar/TabBar"
 import Input from "../components/Input/Input";
 import Button from "../components/Button/Button";
 import { icon_svgs } from "../components/Icon/data";
 
 const URLbox = styled(Flexbox)`
-
+background: ${colors.Background_White};
+border: 2px solid ${colors.DarkGrey};
+border-radius: 50px;
+width: 100%;
 `
 
 export default function Sandbox() {
-  return (
-    <Flexbox>
-      <NavBar></NavBar>
-      <Wrapper>
-        <Header text="Upload your study materials!"></Header>
-        <SubHeader text="Enter URL or upload files, we will make it easier to understand for you."></SubHeader>
-        <TabBar></TabBar>
-        <URLbox dir="row">
-          <Input width ="100%" ></Input>
-          <Button text = "Customize" icon_name = "chevronDown" type = "IconButton"/>
-        </URLbox>
-        <Button text ="upload" type ="default" />
-      </Wrapper>
-    </Flexbox>
-  )
+    return (
+        <Flexbox>
+            <NavBar></NavBar>
+            <Wrapper>
+                <Header text="Upload your study materials!"></Header>
+                <SubHeader text="Enter URL or upload files, we will make it easier to understand for you."></SubHeader>
+                <TabBar></TabBar>
+                <URLbox dir="row">
+                    <Input border="none" borderRadius="3.125rem 0 0 3.125rem;" width="100%" ></Input>
+                    <Button borderRadius="0 3.125rem 3.125rem 0;" text="Customize" icon_name="chevronDown" type="IconButton" />
+                </URLbox>
+
+            </Wrapper>
+            <Button text="upload" type="default" />
+        </Flexbox>
+    )
 }
