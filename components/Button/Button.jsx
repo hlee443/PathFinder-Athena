ecab65936c09d6940b1ebde80725d61fef632f04
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-18 20:14:27 -0700

    and tabbar button type

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,49 +1,55 @@
 import styled from "styled-components";
 import Icon from "../Icon/Icon";
+import { icon_svgs } from "../Icon/data";
+import { colors } from "../../styles/globals";
+import { btn_data } from "../TabBar/data";
 
 const ButtonCont = styled.button`
-  background-color: ${(props) => props.backgroundColor};
-  border-radius: ${(props) => props.borderRadius};
-  border: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
-  font-size: ${(props) => props.fontSize};
-  `
+border: none;
+display: flex;
+align-items: center;
+justify-content: space-evenly;
+background-color: ${(props) => props.backgroundColor};
+border-radius: ${(props) => props.borderRadius || "3.125rem"};
+border-bottom: ${(props) => props.borderBottom};
+width: ${(props) => props.width || "15rem"};
+height: ${(props) => props.height};
+font-size: ${(props) => props.fontSize};
+border-color: ${(props) => props.borderColor};
+`
 
 export default function Button({
   backgroundColor = "#96ADFC",
   text = "button text",
-  width = "100%",
   height = "100%",
-    fontSize = "1rem",
-    textColor={textColor}
+  fontSize = "1rem",
+  textColor = "black",
+  borderBottom = "none",
+  borderRadius = "3.125rem",
+  borderColor = "none",
+  type = "btn type",
+  icon_name = "link",
+  handleClick = () => {}
 }) {
 
-  var src = "/Link.svg"
-
-  const handleClick = () => {
-    console.log("hi!");
-  };
-
-  const changeIcon = (src) => {
-    src = "/Link.svg"
-  };
+  // const handleClick = () => {
+  //   console.log("hi!");
+  // };
 
   return (
-    <ButtonCont
-      type="button"
-      onClick={handleClick}
-      backgroundColor={backgroundColor}
-      width = {width}
-      height = {height}
-      fontSize={fontSize}
-      textColor={textColor}
-    >
-      {text}
-      <Icon src={changeIcon(src)} />
-    </ButtonCont>
+    <div onClick={handleClick}>
+      {
+        type === "TabBar" &&
+        <ButtonCont
+          borderBottom={borderBottom}
+          backgroundColor="transparent"
+          borderRadius="0"
+        >
+          {text}
+          <Icon size = "2.563rem" changeIcon={icon_svgs[icon_name]} />
+        </ButtonCont>
+      }
+    </div>
+
   );
 }
