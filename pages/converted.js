002ac559b0cf52612694e63a8648803cf952e449
cho196 <91933101+cho196@users.noreply.github.com>
2022-10-19 10:30:38 -0700

    add icon

--- pages/converted.js
+++ pages/converted.js
@@ -2,11 +2,10 @@ import NavBar from "../components/NavBar/NavBar";
 import Header from "../components/Header/Header";
 import SubHeader from "../components/SubHeader/SubHeader";
 import { colors, Flexbox, Wrapper } from "../styles/globals";
-import TabBar from "../components/TabBar/TabBar";
-import Input from "../components/Input/Input";
 import Button from "../components/Button/Button";
 import Toolbar from "../components/Toolbar/Toolbar";
 import Container from "../components/Container/Container";
+import Icon from "../components/Icon/Icon";
 import { icon_svgs } from "../components/Icon/data";
 
 export default function Converted() {
@@ -15,6 +14,7 @@ export default function Converted() {
       <NavBar></NavBar>
       <Wrapper>
         <Header text="Assignment"></Header>
+        <Icon></Icon>
         <Toolbar></Toolbar>
         <Container>
           <SubHeader></SubHeader>
