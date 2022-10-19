f588c864f2fd9943c3749ee9b6256368f85c096b
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-18 18:17:45 -0700

    add button props

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -3,19 +3,22 @@ import Icon from "../Icon/Icon";
 
 const ButtonCont = styled.button`
   background-color: ${(props) => props.backgroundColor};
-  border-radius: 2rem;
+  border-radius: ${(props) => props.borderRadius};
   border: none;
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 15rem;
-  height: 4rem;
-  font-size: 1.125rem;
-`;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font-size: ${(props) => props.fontSize};
+  `
 
 export default function Button({
   backgroundColor = "#96ADFC",
   text = "button text",
+  width = "100%",
+  height = "100%",
+  fontSize = "1rem",
 }) {
 
   const handleClick = () => {
@@ -27,6 +30,10 @@ export default function Button({
       type="button"
       onClick={handleClick}
       backgroundColor={backgroundColor}
+      width = {width}
+      height = {height}
+      fontSize={fontSize}
+      textColor={textColor}
     >
       {text}
       <Icon src="" />
