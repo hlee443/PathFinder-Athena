5d62362928d57155943cc216456b548b603e2c82
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-19 15:53:32 -0700

    structure fix

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,25 +1,39 @@
 import styled from "styled-components";
+import { Flexbox } from "../../styles/globals";
 import { icon_svgs } from "./data";
 
-const IconCont = styled.img`
+const IconCont = styled(Flexbox)`
+align-self: ${(props) => props.align};
+width: ${(props) => props.size};
+height: ${(props) => props.size};
+`
+
+const IconImg = styled.img`
   background-color: ${(props) => props.backgroundColor};
-  width: ${(props) => props.size};
-  height: ${(props) => props.size};
+    width: 100%;
+    height:100%;
 `;
+
+
 
 export default function Icon({
   backgroundColor = "transparent",
   size = "3rem",
   icon_name = "sound",
-  changeIcon = () => { }
+  changeIcon = () => { },
+  align = "",
+  handleClick = () => { }
 }) {
 
   return (
-    <IconCont
-      src={changeIcon}
-      name={icon_name}
-      size={size}
-      backgroundColor={backgroundColor}
-    ></IconCont>
+    <IconCont align={align} size={size} onClick={handleClick}>
+      <IconImg
+        src={changeIcon}
+        name={icon_name}
+
+        backgroundColor={backgroundColor}
+      ></IconImg>
+    </IconCont>
+
   );
 }
