05c6d6345c15fe1012c65a5b58d8ff0b6a3b7046
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-18 20:54:57 -0700

    test main page

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -6,6 +6,7 @@ import { Flexbox, Wrapper } from "../styles/globals";
 import TabBar from "../components/TabBar/TabBar"
 import Input from "../components/Input/Input";
 import Button from "../components/Button/Button";
+import { icon_svgs } from "../components/Icon/data";
 
 const URLbox = styled(Flexbox)`
 
@@ -20,10 +21,10 @@ export default function Sandbox() {
         <SubHeader text="Enter URL or upload files, we will make it easier to understand for you."></SubHeader>
         <TabBar></TabBar>
         <URLbox dir="row">
-          <Input></Input>
-          <Button />
+          <Input width ="100%" ></Input>
+          <Button text = "Customize" icon_name = "chevronDown" type = "IconButton"/>
         </URLbox>
-        <Button />
+        <Button text ="upload" type ="default" />
       </Wrapper>
     </Flexbox>
   )
