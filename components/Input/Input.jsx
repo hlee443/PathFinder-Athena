e65b84eeb28cbd2c53387b18880b064918934078
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-19 21:39:22 -0700

    fix input

--- components/Input/Input.jsx
+++ components/Input/Input.jsx
@@ -1,14 +1,21 @@
 import styled from "styled-components";
 import { colors } from "../../styles/globals";
 
-const InputCont = styled.input`
+const InputCont = styled.div`
+width: 100%;
+`
+
+
+const TextInput = styled.input`
   border-radius: ${(props) => props.borderRadius || "3.125rem"};
   border: 0.05rem solid ${colors.darkGray};
   width: ${(props) => props.width || "100%"};
   height: 3.875rem;
   padding: 1rem;
+  width: 100%;
 `;
-const OptionCont = styled.select`
+
+const OptionInput = styled.select`
   border-radius: 2rem;
   border: 0.05rem solid #3e3e3e;
   width: ${(props) => props.width};
@@ -17,30 +24,24 @@ const OptionCont = styled.select`
 `;
 
 export default function Input({
-  width = "15rem",
+  width = "100%",
   type = "text",
-  borderRadius = "",
+  borderRadius = "3.125rem",
   placeholder = "placeholder",
 }) {
-  if (type === "text") {
-    return (
-      <InputCont
-        borderRadius="3.125rem 0 0 3.125rem;"
-        type={type}
-        placeholder={placeholder}
-        width={width}
-      ></InputCont>
-    );
-  }
-
-  if (type === "dropdown") {
-    return (
-      <OptionCont OptionCont type="option" width={width}>
+  return <InputCont>
+    {
+      type === "dropdown" ? <OptionInput OptionCont type="option" width={width}>
         <option value="" selected>
           Placeholder
         </option>
         <option value=""></option>
-      </OptionCont>
-    );
-  }
+      </OptionInput> : <TextInput
+        borderRadius={borderRadius}
+        type={type}
+        placeholder={placeholder}
+        width={width}
+      ></TextInput>
+    }
+  </InputCont>
 }
