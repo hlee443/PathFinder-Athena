24272f0c35cafb4ec873141928bf396d916013ee
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-20 10:05:39 -0700

    import font awesome icon

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,39 +1,12 @@
-import styled from "styled-components";
-import { iconSvgs } from "./data";
-import { Flexbox } from "../../styles/globals";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faLink } from "@fortawesome/free-solid-svg-icons"
-
-
-const IconCont = styled(Flexbox)`
-align-self: ${(props) => props.align};
-width: ${(props) => props.size};
-height: ${(props) => props.size};
-`
-
-const IconImg = styled.div`
-  background-color: ${(props) => props.backgroundColor};
-    width: 100%;
-    height:100%;
-`;
-
-
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Icon({
-  backgroundColor = "transparent",
-  size = "3rem",
-  iconName = "sound",
   faIconName = faLink,
-  changeIcon = () => { },
-  handleClick = () => {},
-  align = ""
+  handleClick = () => { },
+  size = "lg"
 }) {
   return (
-    <IconCont align={align} size={size} onClick={handleClick}>
-      <IconImg
-        name={iconName}
-        backgroundColor={backgroundColor}
-      ><FontAwesomeIcon icon={faIconName}></FontAwesomeIcon></IconImg>
-    </IconCont>
+    <FontAwesomeIcon size={size} onClick={handleClick} icon={faIconName}></FontAwesomeIcon>
   );
-}
+};
