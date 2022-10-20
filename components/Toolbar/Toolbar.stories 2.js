63e3249b20139a5c0487bb0f91ebae84dc378253
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-20 11:28:50 -0700

    edit toolbar

--- components/Toolbar/Toolbar.stories 2.js
+++ components/Toolbar/Toolbar.stories 2.js
@@ -1,14 +1,14 @@
-import Toolbar from "./Toolbar.jsx";
+import ToolBar from "./ToolBar.jsx";
 
 // The default export metadata controls how Storybook lists your stories and provides information used by addons.
 // https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Toolbar",
-  component: Toolbar,
+  title: "ToolBar",
+  component: ToolBar,
 };
 
 // Any other named exports will be treated as stories, which should be functions that return your component
-const Template = (args) => <Toolbar {...args} />;
+const Template = (args) => <ToolBar {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
