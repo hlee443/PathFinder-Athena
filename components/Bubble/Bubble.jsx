20893e9a95df63600c3a4661100d7ab4677ab6ae
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-20 14:14:21 -0700

    add close bubble function

--- components/Bubble/Bubble.jsx
+++ components/Bubble/Bubble.jsx
@@ -4,16 +4,16 @@ import Header from "../Header/Header";
 import Input from "../Input/Input";
 import SubHeader from "../SubHeader/SubHeader";
 import Button from "../Button/Button";
-import { iconSvgs } from "../Icon/data.js";
-import { Flexbox } from "../../styles/globals";
+import { colors, Flexbox } from "../../styles/globals";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const BubbleCont = styled(Flexbox)`
-max-width: ${(props) => props.width};
-max-height: ${(props) => props.height};
+max-width: 60rem;
+max-height: 42rem;
 border-radius: 2rem;
 background-color: #fffef6;
-padding: 4rem;
+padding: 2.5rem;
 z-index: 1;
 position: absolute;
 top: 50%;
@@ -23,14 +23,9 @@ align-items: flex-start;
 justify-content: space-around;
 `;
 
-const ButtonCont = styled(Flexbox)`
+const BtnCont = styled(Flexbox)`
 justify-content: space-between;
 width: 100%;
-`;
-
-const InputDiv = styled(Flexbox)`
-width: 100%;
-height: 100%;
 `;
 
 const CloseButton = styled.div`
@@ -38,24 +33,33 @@ display: flex;
 align-self: end;
 `;
 
-
 export default function Bubble({
-    width = "60rem",
+    active = null,
+    setActive = null,
     type = "login",
-    height = "42rem"
 }) {
+
+    const [header, setHeader] = useState("Header Text");
+    const [subHeader, setSubHeader] = useState("Subheader text");
+
+    const closeBubble = () => {
+        setActive(!active);
+    };
 
     return (
         <BubbleCont>
             <CloseButton>
-                <Icon faIconName ={faClose} />
+                <Icon handleClick={closeBubble} faIconName={faClose} />
             </CloseButton>
-            <Header></Header>
-            <Input></Input>
-            <Input></Input>
-            <SubHeader></SubHeader>
-            <Button></Button>
-            <Button></Button>
+            <Header text={header}></Header>
+            <Input width="100%"></Input>
+            <Input width="100%"></Input>
+            {type === "signup" && <Input width="100%"></Input>}
+            <SubHeader text={subHeader}></SubHeader>
+            <BtnCont dir="row">
+                <Button text="Cancel" handleClick={closeBubble} backgroundColor={colors.buttonPrimaryBlue}></Button>
+                <Button text="Log In" ></Button>
+            </BtnCont>
         </BubbleCont>
     );
 };
