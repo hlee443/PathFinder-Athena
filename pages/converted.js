dfb7d751281387f2b6558d336f732f444b8fa9aa
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-21 11:34:05 -0700

    fix container

--- pages/converted.js
+++ pages/converted.js
@@ -1,9 +1,8 @@
 import NavBar from "../components/NavBar/NavBar";
 import Header from "../components/Header/Header";
 import SubHeader from "../components/SubHeader/SubHeader";
-import { colors, Flexbox, Wrapper } from "../styles/globals";
+import { colors, Flexbox, Wrapper, Container } from "../styles/globals";
 import Toolbar from "../components/Toolbar/Toolbar";
-import Container from "../components/Container/Container";
 import Icon from "../components/Icon/Icon";
 import styled from "styled-components";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
@@ -12,20 +11,19 @@ import { faPencil } from "@fortawesome/free-solid-svg-icons";
 const Title = styled(Flexbox)``
 
 export default function Converted() {
-    return (
-        <Flexbox>
-            <NavBar></NavBar>
-            <Wrapper>
-                <Title dir="row">
-                    <Header text="Assignment"></Header>
-                    <Icon faIconName={faPencil}></Icon>
-                </Title>
-
-                <Toolbar></Toolbar>
-                <Container>
-                    <SubHeader></SubHeader>
-                </Container>
-            </Wrapper>
-        </Flexbox>
-    );
+  return (
+    <Flexbox>
+      <NavBar></NavBar>
+      <Wrapper>
+        <Title dir="row">
+          <Header text="Assignment"></Header>
+          <Icon faIconName={faPencil}></Icon>
+        </Title>
+        <Toolbar></Toolbar>
+        <Container width="100%" height="100%">
+          //converted text goes here
+        </Container>
+      </Wrapper>
+    </Flexbox>
+  );
 }
