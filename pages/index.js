816ed3944ae5c1241528b0d68bff34e5f2ed30ee
seohyun9672 <ajeong4@my.bcit.ca>
2022-10-21 19:26:41 -0700

    work on tabbar functionalities

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import Header from "../components/Header/Header";
 import SubHeader from "../components/SubHeader/SubHeader";
 import { colors, Flexbox, Wrapper, Container } from "../styles/globals";
 import TabBar from "../components/TabBar/TabBar";
-import Input from "../components/Input/Input";
 import Button from "../components/Button/Button";
 import { faChevronDown, faUpload } from "@fortawesome/free-solid-svg-icons"
 import Icon from "../components/Icon/Icon";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import Input from "../components/Input/Input";
 
 const URLbox = styled(Flexbox)`
   background: ${colors.Background_White};
@@ -20,35 +21,51 @@ const URLbox = styled(Flexbox)`
 
 export default function Home() {
 
+  const r = useRouter();
+  const [inputType, setInputType] = useState("url");
+
+  // const changeInputType = (newInputType) => {
+  //   if (newInputType === "url") {
+  //     setInputType("url");
+  //   }
+  //   else if (newInputType === "upload") {
+  //     setInputType("upload");
+  //   }
+  // }
+
   return (
     <Flexbox>
       <NavBar></NavBar>
       <Wrapper>
         <Header text="Upload your study materials!"></Header>
         <SubHeader text="Enter URL or upload files, we will make it easier to understand for you."></SubHeader>
-        <TabBar></TabBar>
-        <URLbox dir="row">
-          <Input
-            border="none"
-            borderRadius="3.125rem 0 0 3.125rem;"
-            width="100%"
-            placeholder="Paste your URL here.."
-          ></Input>
-          <Button
-            borderRadius="0 3.125rem 3.125rem 0;"
-            text="Customize"
-            type="IconButton"
-            ButtonFaIconName={faChevronDown}
-          />
-        </URLbox>
-        <Container width="100%">
-          <Icon faIconName={faUpload} ></Icon>
-          <SubHeader text="Drag and drop a file here"></SubHeader>
-          <p>or</p>
-          <Button text="Choose a file"></Button>
-        </Container>
+        <TabBar changePage ={() => setInputType("url")}></TabBar>
+        {
+          inputType === "url" && <URLbox dir="row">
+            <Input
+              border="none"
+              borderRadius="3.125rem 0 0 3.125rem;"
+              width="100%"
+              placeholder="Paste your URL here.."
+            ></Input>
+            <Button
+              borderRadius="0 3.125rem 3.125rem 0;"
+              text="Customize"
+              type="IconButton"
+              ButtonFaIconName={faChevronDown}
+            />
+          </URLbox>
+        }
+        {
+          inputType === "upload" && <Container width="100%">
+            <Icon faIconName={faUpload} ></Icon>
+            <SubHeader text="Drag and drop a file here"></SubHeader>
+            <p>or</p>
+            <Button text="Choose a file"></Button>
+          </Container>
+        }
       </Wrapper>
-      <Button text="upload" type="default" />
+      {inputType === "url" && <Button text="upload" type="default" />}
     </Flexbox>
   );
 };
