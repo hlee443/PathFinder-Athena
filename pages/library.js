1fc16d7545d3f1f34583fb39b6c011e8034dc2b5
kongdunk <daesan98@gmail.com>
2022-10-22 20:02:23 -0700

    spacing changed

--- pages/library.js
+++ pages/library.js
@@ -12,13 +12,17 @@ const FileDisplay = styled(Flexbox)`
 width: 100%;
 `
 
+const FeatureExplainText = styled(Flexbox)`
+padding: 6rem;
+`
+
 const FeatureExplainCont = styled(Flexbox)`
-width: 100%;
 flex-direction:row;
 justify-content: space-between;
 padding: 6rem;
 flex-wrap: wrap;
 align-self: flex-start;
+margin-top: -5rem;
 `
 
 export default function library() {
@@ -31,50 +35,53 @@ export default function library() {
                     <File></File>
                     <BodyText>Your library is currently empty, add a document to get started.</BodyText>
                 </FileDisplay>
+            </Wrapper>
+            
+            <FeatureExplainText>
                 <Header text="Customize your documents to your needs"/>
                 <SubHeader text="Pathfinder gives you the tools to have a better experiance accessing documents. Use the tools in the toolbar to customize and access documents"/>
-            </Wrapper>
-                <FeatureExplainCont>
-                    <FeatureExplain 
-                        featureName="Save" 
-                        icon={faBookmark} 
-                        featureDescription="Save the document to the library"
-                    />
-                    <FeatureExplain 
-                        featureName="Summarize" 
-                        icon={faFileLines}
-                        featureDescription="Summarizes lengthy and complicated texts"
-                    />
-                    <FeatureExplain 
-                        featureName="Highlight" 
-                        icon={faHighlighter}
-                        featureDescription="Highlight texts"    
-                    />
-                    <FeatureExplain 
-                        featureName="Download" 
-                        icon={faDownload}
-                        featureDescription="Download the document"    
-                    />
-                    <FeatureExplain 
-                        featureName="Text-to-speech" 
-                        icon={faVolumeHigh}
-                        featureDescription="Turn text into natural speech"
-                    />
-                    <FeatureExplain 
-                        featureName="Text Settings" 
-                        icon={faFont}
-                        featureDescription="Adjust the settings of the text in the document"
-                    />
-                    <FeatureExplain 
-                        featureName="Dictionary" 
-                        icon={faMagnifyingGlass}
-                        featureDescription="Look up the meaning of English words"    
-                    />
-                    <FeatureExplain 
-                        featureName="More to come..."
-                        icon={faSquarePlus} 
-                        featureDescription=""
-                    />
-                </FeatureExplainCont>
+            </FeatureExplainText>
+            <FeatureExplainCont>
+                <FeatureExplain 
+                    featureName="Save" 
+                    icon={faBookmark} 
+                    featureDescription="Save the document to the library"
+                />
+                <FeatureExplain 
+                    featureName="Summarize" 
+                    icon={faFileLines}
+                    featureDescription="Summarizes lengthy and complicated texts"
+                />
+                <FeatureExplain 
+                    featureName="Highlight" 
+                    icon={faHighlighter}
+                    featureDescription="Highlight texts"    
+                />
+                <FeatureExplain 
+                    featureName="Download" 
+                    icon={faDownload}
+                    featureDescription="Download the document"    
+                />
+                <FeatureExplain 
+                    featureName="Text-to-speech" 
+                    icon={faVolumeHigh}
+                    featureDescription="Turn text into natural speech"
+                />
+                <FeatureExplain 
+                    featureName="Text Settings" 
+                    icon={faFont}
+                    featureDescription="Adjust the settings of the text in the document"
+                />
+                <FeatureExplain 
+                    featureName="Dictionary" 
+                    icon={faMagnifyingGlass}
+                    featureDescription="Look up the meaning of English words"    
+                />
+                <FeatureExplain 
+                    featureName="More to come..."
+                    icon={faSquarePlus} 
+                    featureDescription=""
+                />
+            </FeatureExplainCont>
         </Flexbox>)
 }
