4f30a9212e7feebf6350692493e2010dd0b3652a
Laurent Beique <lbeique@outlook.com>
2022-10-24 20:18:48 -0700

    quickfix

--- components/Input/Input.jsx
+++ components/Input/Input.jsx
@@ -39,7 +39,7 @@ export default function Input({
         placeholder={placeholder}
         width={width}
         onChange={(e) => setInputValue(e.target.value)}
-        value={inputValue}
+        defaultValue={inputValue}
         border={border}
         bgColor={bgColor}
       ></InputCont>
