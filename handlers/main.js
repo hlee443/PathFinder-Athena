cb07a5fa1f4282c5661219dbfcdba6f454889bef
Laurent Beique <lbeique@outlook.com>
2022-10-25 04:58:50 -0700

    db handlers for athena

--- handlers/main.js
+++ handlers/main.js
@@ -1,6 +1,22 @@
 // Main Handlers
 
+// Features
 export * from "./features/download";
 export * from "./features/summarize";
 export * from "./features/upload";
 export * from "./features/dictionary";
+
+// Database
+
+// Users
+export * from "./database/getUserById";
+export * from "./database/getUserByLogin";
+export * from "./database/addUser";
+
+// Settings
+
+// Files
+
+// Folders
+
+// Images
