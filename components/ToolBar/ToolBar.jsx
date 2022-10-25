b00c8e920603e08028cdaba0e33bf5618e2bd277
Ivy Lee <hlee443@my.bcit.ca>
2022-10-25 11:34:27 -0700

    remove old dictionary toggle

--- components/ToolBar/ToolBar.jsx
+++ components/ToolBar/ToolBar.jsx
@@ -10,7 +10,6 @@ import {
   faBookmark,
   faDownload,
 } from "@fortawesome/free-solid-svg-icons";
-import * as mainHandler from "../../handlers/main";
 import ToolBarDropdown, {
   DictionaryDropdown,
 } from "../ToolBarDropdown/ToolBarDropdown";
@@ -28,30 +27,7 @@ const ToolBarCont = styled.div`
 export default function ToolBar() {
   const [showLibrary, setShowLibrary] = useState(false);
   const [showFont, setShowFont] = useState(false);
-  const [wordInfo, setWordInfo] = useState(null);
-  /**
-   *
-   *
-   * Fetch word information
-   */
-  function fetchInfo(e) {
-    e.preventDefault();
-    try {
-      const word = window.getSelection().toString();
-      // callback 
-      mainHandler.handleDictionary(word, (res) => {
-        const { data } = res;
-        const { definition } = data;
-        console.log("RES", res);
-        // split the response string into an array using regex
-        const newDefinition = definition.split(/1. |2. | 3. /);
 
-        setWordInfo(newDefinition);
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  }
 
   return (
     <ToolBarCont>
@@ -63,35 +39,8 @@ export default function ToolBar() {
       <Icon
         faIconName={faMagnifyingGlass}
         text="Dictionary"
-        handleClick={fetchInfo}
         hoverColor={colors.backgroundYellow}
       ></Icon>
-      {wordInfo && (
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>
-                  <span>{window.getSelection().toString()}</span>
-                </th>
-                <th>
-                  <span>Definition</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1.</td>
-                <td>{wordInfo[1]}</td>
-              </tr>
-              <tr>
-                <td>2.</td>
-                <td>{wordInfo[2]}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      )}
       <Icon
         faIconName={faFileLines}
         text="Summarize"
