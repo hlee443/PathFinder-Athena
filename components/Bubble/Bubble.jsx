902bb069e6ecc15c3ee0c7780860618a6009ead0
cho196 <91933101+cho196@users.noreply.github.com>
2022-10-25 23:28:01 -0700

    add overlay background for bubbles

--- components/Bubble/Bubble.jsx
+++ components/Bubble/Bubble.jsx
@@ -10,16 +10,16 @@ import { useState } from "react";
 import { bubbleData } from "./data";
 
 const BubbleCont = styled(Flexbox)`
-  max-width: 60rem;
+  max-width: 50rem;
   max-height: 42rem;
   border-radius: 2rem;
   background-color: #fffef6;
   padding: 2.5rem;
   z-index: 1;
-  position: absolute;
-  top: 100%;
-  left: 20%;
-  right: 20%;
+  // position: absolute;
+  // top: 100%;
+  // left: 20%;
+  // right: 20%;
   align-items: flex-start;
   justify-content: space-around;
 `;
@@ -41,26 +41,25 @@ const SubHeaderCont = styled(Flexbox)`
 
 const InputCont = styled(Flexbox)`
   width: 100%;
-`
+`;
 
 export default function Bubble({
   type = "login",
-  onClose = () => { },
-  handleBubble = () => { },
+  onClose = () => {},
+  handleBubble = () => {},
 }) {
-
   return (
-    <BubbleCont type ={type}>
+    <BubbleCont type={type}>
       <CloseButton>
         <Icon handleClick={onClose} faIconName={faClose} />
       </CloseButton>
       <Header text={bubbleData[type].header} />
-      {
-        (type == "login" || type === "signup") && <InputCont>
-        <Input width="100%" />
-        <Input width="100%" />
-      </InputCont>
-      }
+      {(type == "login" || type === "signup") && (
+        <InputCont>
+          <Input width="100%" />
+          <Input width="100%" />
+        </InputCont>
+      )}
       <SubHeaderCont dir="row">
         {type === "signup" && <Icon faIconName={faSquare} />}
         <SubHeader text={bubbleData[type].subHeader1} />
@@ -79,4 +78,4 @@ export default function Bubble({
       {type === "signup" && <SubHeader text={bubbleData[type].subHeader2} />}
     </BubbleCont>
   );
-};
+}
