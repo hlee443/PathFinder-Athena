e6e2b1287ab302d07c73e6a913a64a47802984d8
seohyun9672 <ajeong4@my.bcit.ca>
2022-11-16 09:35:13 -0800

    fix hover

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -2,48 +2,54 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import styled from "styled-components";
-import { colors, Flexbox, textData } from "../../styles/globals";
+import { colors, Flexbox } from "../../styles/globals";
+import useMediaQuery from "../../MediaQuery/MediaQuery";
+import { mediaQuery } from "../../MediaQuery/data";
+import Label from "../Label/Label";
 
 const IconCont = styled(Flexbox)`
   padding: 0.5rem;
   gap: 0.5rem;
   cursor: pointer;
-  position: relative;
+  // position: relative;
   height: 100%;
   width: ${(props) => props.width};
-  :hover {
-  background-color: ${(props) => props.hoverColor};
   border-radius: 1.25rem;
+  justify-content: space-between;
+
+  :hover {
+    background-color: ${(props) => props.hoverColor};
 }
 `;
 
-
-
 const IconImg = styled.img`
   aspect-ratio: 1;
+  width: ${(props) => props.size};
 `;
 
 export default function Icon({
   faIconName = faLink,
   handleClick = () => { },
-  size = "lg",
+  size = "",
   color = colors.textBlack,
   hoverColor = "transparent",
   src = null,
   text = "",
-  width= "",
-  handleMouseEnter = () => {},
-  handleMouseLeave = () => {},
+  width = "",
+  handleMouseEnter = () => { },
+  handleMouseLeave = () => { },
 }) {
 
-  const [label, setLabel] = useState(false);
   const [bg, setBg] = useState("transparent");
+  const isMobile = useMediaQuery(mediaQuery.maxWidth.mobile);
 
   return (
     <IconCont
       onClick={handleClick}
       hoverColor={hoverColor}
       width={width}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {src === null && (
         <FontAwesomeIcon
@@ -52,7 +58,7 @@ export default function Icon({
           icon={faIconName}
         />
       )}
-      {src !== null && <IconImg src={src} />}
+      {src !== null && <IconImg size={isMobile ? "2rem" : size} src={src} />}
       {text && <div>{text}</div>}
     </IconCont>
   );
