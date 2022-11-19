e7e7bdbe7796e3ef0f34f3aeba1e2880048b7f31
seohyun9672 <ajeong4@my.bcit.ca>
2022-11-18 20:00:48 -0800

    fix style

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import Icon from "../Icon/Icon";
-import { colors, Flexbox, textData } from "../../styles/globals";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { colors, textData } from "../../styles/globals";
 import { btnData } from "./data";
 
 const StyledButton = styled.button`
@@ -15,14 +14,19 @@ const StyledButton = styled.button`
   border-bottom: ${(props) => props.borderBottom};
   max-width: 15rem;
   max-height: 3.875rem;
-  width: ${(props) => props.width || "15rem"};
-  min-height: ${(props) => props.height || "3.875rem"};
+  height: ${(props) => props.height};
+  width: ${(props) => props.width};
+  min-width: "fit-content";
+  white-space: nowrap;
+  min-height: "3.875rem";
   font-size: ${(props) => props.fontSize};
   border-color: ${(props) => props.borderColor};
   line-height: ${textData.lineHeight};
   letter-spacing: ${textData.letterSpacing};
   cursor: pointer;
-  color: ${colors.textBlack}
+  color: ${(props) => props.color || colors.textBlack};
+  padding: 0.5rem;
+
   :hover {
     background-color: ${(props) => props.hoverColor || btnData.state.hover};
     font-weight: bolder;
@@ -30,37 +34,40 @@ const StyledButton = styled.button`
 `;
 
 export default function Button({
-  backgroundColor = "#96ADFC",
+  color="",
+  backgroundColor = "",
   text = "button text",
-  height = "3.875rem",
-  fontSize = "1rem",
+  height = "",
+  // fontSize = "1rem",
   borderBottom = "none",
   borderRadius = "3.125rem",
-  type = "btn type",
-  handleClick = () => {},
-  width = "15rem",
-  faIconName = faLink,
+  handleClick = () => { },
+  width = "",
+  faIconName = null,
   fontWeight = "normal",
-  iconSize = "2x",
-  hoverColor = btnData.state.hover,
+  iconSize = "1x",
+  iconColor = "",
+  handleMouseEnter = () => { },
+  handleMouseLeave = () => { },
 }) {
+
   return (
     <StyledButton
+      color={color}
       onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       backgroundColor={backgroundColor}
       text="button text"
       height={height}
-      fontSize={fontSize}
+      // fontSize={fontSize}
       borderBottom={borderBottom}
       borderRadius={borderRadius}
-      type="btn type"
       width={width}
       fontWeight={fontWeight}
-      iconSize={iconSize}
-      hoverColor={hoverColor}
     >
       {text}
-      {type === "IconButton" && <Icon faIconName={faIconName} />}
+      {faIconName !== null && <Icon color={iconColor} size={iconSize} faIconName={faIconName} />}
     </StyledButton>
   );
 }
