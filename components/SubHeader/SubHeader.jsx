9f069d4b8ae9f79a5a518283c6e5aec9db14b4b8
kongdunk <daesan98@gmail.com>
2022-11-20 01:31:24 -0800

    SubHeader fade-in added

--- components/SubHeader/SubHeader.jsx
+++ components/SubHeader/SubHeader.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { textData } from "../../styles/globals";
 import { mediaQuery } from "../../MediaQuery/data";
-
+import { motion } from "framer-motion";
 
 const SubHeaderCont = styled.h2`
   font-size: ${textData.h2.size};
@@ -17,5 +17,15 @@ const SubHeaderCont = styled.h2`
   `
 
 export default function SubHeader({ text = "SubHeader text" }) {
-  return <SubHeaderCont>{text}</SubHeaderCont>;
+  return <>
+    <motion.div
+    initial={{ opacity: 0, scale: 1.2 }}
+    animate={{ opacity: 1, scale:1 }}
+    transition={{ duration: 1.2 }}
+    >
+
+      <SubHeaderCont>{text}</SubHeaderCont>;
+
+    </motion.div>
+    </>
 };
