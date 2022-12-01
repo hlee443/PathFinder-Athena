f96c5f75ef95122df31d68fdc67274d307119fe8
cho196 <91933101+cho196@users.noreply.github.com>
2022-12-01 13:06:56 -0800

    about page

--- pages/about.js
+++ pages/about.js
@@ -18,6 +18,14 @@ import { useRouter } from "next/router";
 //   },
 // ];
 
+const StyledWrapper = styled(Wrapper)`
+  padding: 10rem;
+`;
+
+const StyledImg = styled.img`
+  max-width: 15rem;
+`;
+
 export default function About() {
   const router = useRouter();
 
@@ -25,10 +33,50 @@ export default function About() {
     <Flexbox>
       <LogoBar />
       <NavBar />
-      <Wrapper>
+      <StyledWrapper>
+        <StyledImg src="../images/icon.svg"></StyledImg>
         <Header text="Pathfinder" />
-        <SubHeader text="Let us make your documents easier to understand." />
-      </Wrapper>
+        <SubHeader text="Who We Are" />
+        <p>
+          Pathfinder is a study helper that reformats educational resources such
+          as assignment guidelines and instructions to help students with
+          dyslexia understand the content easily.{" "}
+        </p>
+        <SubHeader text="Our Mission"></SubHeader>
+        <p>
+          Our mission is to help students with dyslexia to find studying easier
+          with a couple of changes. We created an app to enhance learning
+          experiences for everyone.
+        </p>
+        <SubHeader text="Background"></SubHeader>
+        <p>
+          As a group of students studying for coursework every day, we found
+          course guidelines and instructions were sometimes disorganized and
+          hard to understand. Since we, as able individuals, struggled to
+          process them, we imagined that people with dyslexia and other learning
+          disabilities would find it even more difficult.{" "}
+        </p>
+        <SubHeader text="Our Benefits"></SubHeader>
+        <p>
+          Having dyslexia does not mean you are not intelligent. With
+          Pathfinder, dyslexic students can do better with reading and
+          understanding, and do very well in school.
+        </p>
+        <ul>
+          <li>
+            Files are reformatted based on your text, colour, and spacing
+            preference, so they are easier for you to read.
+          </li>
+          <li>
+            The entire website complies with AAA guidelines, so nothing would
+            distract you while navigating our pages.
+          </li>
+          <li>
+            Consistent layout and component placement will help you find the
+            information fast without confusion.
+          </li>
+        </ul>
+      </StyledWrapper>
     </Flexbox>
   );
 }
