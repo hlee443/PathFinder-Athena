19264b31c021424e05fa4f63adf049b9fdb83908
Laurent Beique <lbeique@outlook.com>
2022-12-01 01:56:11 -0800

    highlight-handlers added

---
+++ handlers/database/addHighlight.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export async function handleAddHighlight(dbData, cb) {
+    // dbData = {
+    //     "highlightData": {
+    //         "fileId": "1",
+    //         "highlightUuid": "2dscu-12ad4-azv34-ewr34-1qwev",
+    //     }
+    // }
+
+    try {
+        return await axios({
+            method: "POST",
+            url: `${process.env.NEXT_PUBLIC_HERMES_PATH}/api/db/highlight`,
+            data: dbData
+        })
+            .then((response) => {
+                cb(response)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ handlers/database/deleteHighlight.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export async function handleDeleteHighlight(highlightId, cb) {
+
+    try {
+        return await axios({
+            method: "DELETE",
+            url: `${process.env.NEXT_PUBLIC_HERMES_PATH}/api/db/highlight${highlightId}`,
+            data: dbData
+        })
+            .then((response) => {
+                cb(response)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    } catch (error) {
+        console.log(error)
+    }
+}
