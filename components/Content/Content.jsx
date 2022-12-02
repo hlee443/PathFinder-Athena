78e57baeeeb2eb7f8a776142cc4558c257cb79ae
Callista Stefanie Taswin <callistastefanie@gmail.com>
2022-12-02 00:38:42 -0800

    br fixes

--- components/Content/Content.jsx
+++ components/Content/Content.jsx
@@ -18,10 +18,11 @@ export default function Content({
         //console.log("CONTENT CONVERSION", fileData.file_content)
 
         const isHTML = /<([A-Za-z][A-Za-z0-9]*)\b[^>]*>(.*?)<\/\1>/
+        const isBr =/<br\s*[\/]?>/gi
 
         const fileBody = document.querySelector(".file__content")
         setFileContent(fileData.file_content)
-        if(isHTML.test(fileContent)){
+        if(isHTML.test(fileContent) || isBr.test(fileContent)){
             fileBody.innerHTML = fileContent
         } else {
             fileBody.innerText = fileContent
