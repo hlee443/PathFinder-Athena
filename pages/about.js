21cc23cbbeaf5a1b8cc8111f02b6505e5681158e
cho196 <91933101+cho196@users.noreply.github.com>
2022-12-02 03:19:57 -0800

    about page

--- pages/about.js
+++ pages/about.js
@@ -19,64 +19,128 @@ import { useRouter } from "next/router";
 // ];
 
 const StyledWrapper = styled(Wrapper)`
-  padding: 10rem;
+  padding-left: 10rem;
+  padding-right: 10rem;
   height: 100%;
 `;
 
 const StyledImg = styled.img`
-  max-width: 15rem;
+  max-width: 50%;
+  padding-right: ${(props) => props.paddingRight || null};
+  padding-left: ${(props) => props.paddingLeft || null};
+`;
+
+const FlexCont = styled(Flexbox)`
+  align-items: flex-start;
+`;
+const Text = styled.p`
+  margin-top: 1rem;
+`;
+
+const Yellow = styled.div`
+  padding-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-bottom: 5rem;
+  background-color: ${colors.backgroundYellow};
+  box-shadow: 0 0 0 100vmax ${colors.backgroundYellow};
+  clip-path: inset(0 -100vmax);
+`;
+
+const Card = styled(Flexbox)`
+  flex-direction: column;
+`;
+
+const CardCont = styled(Flexbox)`
+  align-items: flex-end;
 `;
 
 export default function About() {
-  const router = useRouter();
-
   return (
     <Flexbox>
       <LogoBar />
       <NavBar />
       <StyledWrapper>
-        <StyledImg src="../images/icon.svg"></StyledImg>
         <Header text="Pathfinder" />
-        <SubHeader text="Who We Are" />
-        <p>
-          Pathfinder is a study helper that reformats educational resources such
-          as assignment guidelines and instructions to help students with
-          dyslexia understand the content easily.{" "}
-        </p>
-        <SubHeader text="Our Mission"></SubHeader>
-        <p>
-          Our mission is to help students with dyslexia to find studying easier
-          with a couple of changes. We created an app to enhance learning
-          experiences for everyone.
-        </p>
-        <SubHeader text="Background"></SubHeader>
-        <p>
-          As a group of students studying for coursework every day, we found
-          course guidelines and instructions were sometimes disorganized and
-          hard to understand. Since we, as able individuals, struggled to
-          process them, we imagined that people with dyslexia and other learning
-          disabilities would find it even more difficult.{" "}
-        </p>
-        <SubHeader text="Our Benefits"></SubHeader>
-        <p>
+        <Flexbox dir="row">
+          <FlexCont>
+            <SubHeader text="Who We Are" />
+            <Text>
+              Pathfinder is a study helper that reformats educational resources
+              such as assignment guidelines and instructions to help students
+              with dyslexia understand the content easily.
+            </Text>
+          </FlexCont>
+
+          <StyledImg
+            src="../images/macbook-mockup.svg"
+            alt="mockup"
+          ></StyledImg>
+        </Flexbox>
+        <Flexbox>
+          <Yellow bg="bg">
+            <SubHeader text="Why Pathfinder?"></SubHeader>
+            <video width="100%" controls>
+              <source src="vid.mp4" type="video/mp4" />
+            </video>
+            <Text>
+              As a group of students studying for coursework every day, we found
+              course guidelines and instructions were sometimes disorganized and
+              hard to understand. Since we, as able individuals, struggled to
+              process them, we imagined that people with dyslexia and other
+              learning disabilities would find it even more difficult.{" "}
+            </Text>
+          </Yellow>
+        </Flexbox>
+        <Flexbox dir="row">
+          <StyledImg
+            src="../images/billy.svg"
+            alt="confused billy"
+            width="40%"
+            paddingRight="3rem"
+          ></StyledImg>
+          <FlexCont>
+            <SubHeader text="Our Mission"></SubHeader>
+            <Text>
+              Our mission is to help students with dyslexia, like Billy, to find
+              studying easier with a couple of changes. We created an app to
+              enhance learning experiences for everyone.
+            </Text>
+          </FlexCont>
+        </Flexbox>
+        <Text>
           Having dyslexia does not mean you are not intelligent. With
           Pathfinder, dyslexic students can do better with reading and
           understanding, and do very well in school.
-        </p>
-        <ul>
-          <li>
-            Files are reformatted based on your text, colour, and spacing
-            preference, so they are easier for you to read.
-          </li>
-          <li>
-            The entire website complies with AAA guidelines, so nothing would
-            distract you while navigating our pages.
-          </li>
-          <li>
-            Consistent layout and component placement will help you find the
-            information fast without confusion.
-          </li>
-        </ul>
+        </Text>
+        {/* <CardCont dir="row">
+          <Card>
+            <StyledImg src="../images/text-icons.svg" alt="text icons" />
+            <Text>
+              Files are reformatted based on your text, colour, and spacing
+              preference, so they are easier for you to read.
+            </Text>
+          </Card>
+          <Card>
+            <StyledImg
+              src="https://www.w3.org/WAI/wcag2AAA-blue"
+              alt="Level AAA conformance,
+            W3C WAI Web Content Accessibility Guidelines 2.0"
+            />
+            <Text>
+              The entire website complies with AAA guidelines, so nothing would
+              distract you while navigating our pages.
+            </Text>
+          </Card>
+          <Card>
+            <StyledImg src="../images/layout.svg" alt="layout icons" />
+            <Text>
+              Consistent layout and component placement will help you find the
+              information fast without confusion.
+            </Text>
+          </Card>
+        </CardCont> */}
       </StyledWrapper>
     </Flexbox>
   );
